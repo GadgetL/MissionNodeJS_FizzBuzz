85390e610a530d704f7c5ebe30523e761445d661
GadgetL <laura.ramirezcosmes@gmail.com>
2022-05-11 19:07:57 -0500

    refector(test): add linter and fix format

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,22 +1,22 @@
-const Reader = require('../../lib/utils/Reader')
-const ExplorerService = require('../../lib/services/ExplorerService')
+const Reader = require("../../lib/utils/Reader");
+const ExplorerService = require("../../lib/services/ExplorerService");
 
 describe("Test for ExplorerService", () => {
     test("Return a filter list by mission when receive a list an a mission", () => {
-        const explorers = Reader.readJsonFile('explorers.json')
-        const explorerService = ExplorerService.filterByMission(explorers, 'node')
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorerService = ExplorerService.filterByMission(explorers, "node");
         expect(explorerService).toBeDefined();
-    })
+    });
 
     test("Return amount of explorers by mission", () => {
-        const explorers = Reader.readJsonFile('explorers.json')
-        const explorerService = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorerService = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorerService).toBe(10);
-    })
+    });
 
     test("Return explorer username by mission", () => {
-        const explorers = Reader.readJsonFile('explorers.json')
-        const explorerService = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorerService = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorerService).toBeDefined();
-    })
-})
+    });
+});
